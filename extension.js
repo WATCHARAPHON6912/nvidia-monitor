797d553b768efa7d612d93315c4081d982b41448
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-02-24 14:16:30 +0700

    #3  add LC_ALL=C

--- extension.js
+++ extension.js
@@ -61,7 +61,7 @@ class NvidiaMonitorTreeDataProvider {
         } else if (platform === 'linux') {
             nvidiaCommand = 'nvidia-smi --query-gpu=name,utilization.gpu,memory.used,memory.total,temperature.gpu --format=csv,noheader,nounits';
             cpuCommand = 'top -bn1 | grep "Cpu(s)" | awk \'{print $2 + $4}\'';
-            ramCommand = "LC_ALL=C free -m | awk '/Mem:/ {printf \"%.1f \\n%.1f \\n\", $2, $3}'";
+            ramCommand = "LC_ALL=C free -m | awk '/Mem:/ {printf \"%.1f \\n%.1f \\n\", $2, $3}'"; 
         } else {
             console.error('Unsupported OS');
             return;
