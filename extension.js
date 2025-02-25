543a6fa0a3bace5f9f05a5addcdd455abf58b26e
watcharaphon pamayayang <watcharaphon@weaverbase.com>
2025-02-25 15:04:51 +0700

    #1 Support multiple GPUs

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ class NvidiaMonitorTreeDataProvider {
     constructor() {
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-        this.currentData = {};
+        this.currentData = { "gpu": [{ "device": 'N/A', "gpuUsage": 'N/A', "memoryUsage": 'N/A', "memoryTotal": 'N/A', "temperature": 'N/A' }], "cpuUsage": "N/A", "ramUsage": "N/A" };
     }
 
     getTreeItem(element) {
@@ -21,25 +21,34 @@ class NvidiaMonitorTreeDataProvider {
 
     getChildren(element) {
         if (!element) {
-            const currentTime = this.getCurrentTime();
-            const rootItem = [
-                new NvidiaMonitorTreeItem(`${this.currentData.device || 'N/A'}`, vscode.TreeItemCollapsibleState.Collapsed),
-                new NvidiaMonitorTreeItem(`CPU`, vscode.TreeItemCollapsibleState.Collapsed),
-            ];
+            // const currentTime = this.getCurrentTime();
+            // console.log(this.currentData.gpu);
+
+            const rootItem = [];
+
+            for (let i = 0; i < this.currentData.gpu.length; i++) {
+                // console.log(this.currentData.gpu[i]);
+                rootItem.push(new NvidiaMonitorTreeItem(`GPU${String(i)} : ${this.currentData.gpu[i].device || 'N/A'}`, vscode.TreeItemCollapsibleState.Collapsed));
+            }
+            rootItem.push(new NvidiaMonitorTreeItem(`CPU`, vscode.TreeItemCollapsibleState.Collapsed));
+
             return rootItem;
-        }  else if (element.label.startsWith(`${this.currentData.device || 'N/A'}`)) {
-            return [
-                new NvidiaMonitorTreeItem(`GPU: ${this.currentData.usage || 'N/A'} %`, vscode.TreeItemCollapsibleState.None),
-                new NvidiaMonitorTreeItem(`Memory: ${this.currentData.memoryUsage || 'N/A'}`, vscode.TreeItemCollapsibleState.None),
-                new NvidiaMonitorTreeItem(`Temp: ${this.currentData.temperature || 'N/A'}`, vscode.TreeItemCollapsibleState.None)
-            ];
-        } 
-        else if (element.label.startsWith(`CPU`)) {
+        }
+        for (let i = 0; i < this.currentData.gpu.length; i++) {
+            if (element.label.startsWith(`GPU${String(i)} : ${this.currentData.gpu[i].device || 'N/A'}`)) {
+                return [
+                    new NvidiaMonitorTreeItem(`GPU: ${this.currentData.gpu[i].gpuUsage || 'N/A'} %`, vscode.TreeItemCollapsibleState.None),
+                    new NvidiaMonitorTreeItem(`Memory: ${this.currentData.gpu[i].memoryUsage || 'N/A'}`, vscode.TreeItemCollapsibleState.None),
+                    new NvidiaMonitorTreeItem(`Temp: ${this.currentData.gpu[i].temperature || 'N/A'}`, vscode.TreeItemCollapsibleState.None)
+                ];
+            }
+        }
+        if (element.label.startsWith(`CPU`)) {
             return [
                 new NvidiaMonitorTreeItem(`CPU: ${this.currentData.cpuUsage || 'N/A'} %`, vscode.TreeItemCollapsibleState.None),
                 new NvidiaMonitorTreeItem(`RAM: ${this.currentData.ramUsage || 'N/A'} GB`, vscode.TreeItemCollapsibleState.None),
             ];
-        } 
+        }
         return [];
     }
 
@@ -56,42 +65,43 @@ class NvidiaMonitorTreeDataProvider {
 
         if (platform === 'win32') {
             nvidiaCommand = 'nvidia-smi --query-gpu=name,utilization.gpu,memory.used,memory.total,temperature.gpu --format=csv,noheader,nounits';
+            // nvidiaCommand = "nvidia-smi --query-gpu=name,utilization.gpu,memory.used,memory.total,temperature.gpu --format=csv,noheader,nounits && \
+            // nvidia-smi --query-gpu=name,utilization.gpu,memory.used,memory.total,temperature.gpu --format=csv,noheader,nounits"
+
             cpuCommand = 'wmic cpu get loadpercentage /value';
             ramCommand = 'wmic OS get FreePhysicalMemory , TotalVisibleMemorySize /value';
         } else if (platform === 'linux') {
             nvidiaCommand = 'nvidia-smi --query-gpu=name,utilization.gpu,memory.used,memory.total,temperature.gpu --format=csv,noheader,nounits';
             cpuCommand = 'top -bn1 | grep "Cpu(s)" | awk \'{print $2 + $4}\'';
-            ramCommand = "LC_ALL=C free -m | awk '/Mem:/ {printf \"%.1f \\n%.1f \\n\", $2, $3}'"; 
+            ramCommand = "LC_ALL=C free -m | awk '/Mem:/ {printf \"%.1f \\n%.1f \\n\", $2, $3}'";
         } else {
             console.error('Unsupported OS');
             return;
         }
-
         exec(nvidiaCommand, (error, stdout, stderr) => {
             if (error) {
-                console.error(`Error executing nvidia-smi: ${stderr}`);
-                this.currentData.device = 'Error';
-                this.currentData.usage = 'Error';
-                this.currentData.memoryUsage = 'Error';
-                this.currentData.temperature = 'Error';
+                // console.error(`Error executing nvidia-smi: ${stderr}`);
             } else {
                 const lines = stdout.trim().split('\n');
                 if (lines.length > 0) {
-                    const [device, usage, memoryUsed, memoryTotal, temperature] = lines[0].split(', ');
-                    this.currentData.device = `${device}`;
-                    this.currentData.usage = `${usage}`;
-                    this.currentData.memoryUsage = `${(memoryUsed/ 1024).toFixed(2)} / ${(memoryTotal/ 1024).toFixed(2)}GB`;
-                    this.currentData.temperature = `${temperature}°C`;
-                } else {
-                    this.currentData.device = 'N/A';
-                    this.currentData.usage = 'N/A';
-                    this.currentData.memoryUsage = 'N/A';
-                    this.currentData.temperature = 'N/A';
+                    this.currentData.gpu = []
+                    for (let i = 0; i < lines.length; i++) {
+                        // print(lines[i].split(', '));
+                        const [device, usage, memoryUsed, memoryTotal, temperature] = lines[i].replace("\r", "").split(', ');
+                        this.currentData.gpu.push({
+                            device: device,
+                            gpuUsage: `${usage}`,
+                            memoryUsage: `${(memoryUsed / 1024).toFixed(2)}`,
+                            memoryTotal: `${(memoryTotal / 1024).toFixed(2)}`,
+                            temperature: `${temperature}`
+                        })
+                    }
                 }
             }
 
+
             exec(cpuCommand, (error, stdout, stderr) => {
-                if(platform === 'win32'){
+                if (platform === 'win32') {
                     if (error) {
                         console.error(`Error executing CPU command: ${stderr}`);
                         this.currentData.cpuUsage = 'Error';
@@ -99,33 +109,33 @@ class NvidiaMonitorTreeDataProvider {
                         this.currentData.cpuUsage = `${stdout.trim().split('=')[1]}`;
                     }
                 }
-                else{
+                else {
                     this.currentData.cpuUsage = stdout.trim();
                 }
 
-            exec(ramCommand, (error, stdout, stderr) => {
-                if(platform === 'win32'){
-                    if (error) {
-                        console.error(`Error executing RAM command: ${stderr}`);
-                        this.currentData.ramUsage = 'Error';
+                exec(ramCommand, (error, stdout, stderr) => {
+                    if (platform === 'win32') {
+                        if (error) {
+                            console.error(`Error executing RAM command: ${stderr}`);
+                            this.currentData.ramUsage = 'Error';
+                        } else {
+                            var x = stdout.trim().split('\n');
+                            var free = x[0].split('=')[1];
+                            var total = x[1].split('=')[1]
+                            this.currentData.ramUsage = `${((total - free) / (1024 * 1024)).toFixed(2)}/${(total / (1024 * 1024)).toFixed(2)}`;
+                        }
                     } else {
-                        var x=stdout.trim().split('\n');
-                        var free=x[0].split('=')[1];
-                        var total=x[1].split('=')[1]
-                        this.currentData.ramUsage = `${((total-free)/ (1024 * 1024)).toFixed(2)}/${(total/ (1024 * 1024)).toFixed(2)}`;
+                        if (error) {
+                            console.error(`Error executing RAM command: ${stderr}`);
+                            this.currentData.ramUsage = 'Error';
+                        } else {
+                            var x = stdout.trim().split('\n');
+                            var total = x[0];
+                            var used = x[1];
+                            this.currentData.ramUsage = `${(used / (1024)).toFixed(2)}/${(total / (1024)).toFixed(2)}`;
+                        }
                     }
-                }else{
-                    if (error) {
-                        console.error(`Error executing RAM command: ${stderr}`);
-                        this.currentData.ramUsage = 'Error';
-                    } else {
-                        var x=stdout.trim().split('\n');
-                        var total=x[0];
-                        var used=x[1];
-                        this.currentData.ramUsage = `${(used/ (1024)).toFixed(2)}/${(total/ (1024)).toFixed(2)}`;
-                    }
-                }
-                this._onDidChangeTreeData.fire(); // อัปเดต tree view
+                    this._onDidChangeTreeData.fire(); // อัปเดต tree view
                 });
             });
         });
@@ -140,7 +150,7 @@ function activate(context) {
     console.log('Congratulations, your extension "nvidia-monitor" is now active!');
 
     const nvidiaMonitorTreeDataProvider = new NvidiaMonitorTreeDataProvider();
-    
+
     vscode.window.createTreeView('nvidiaMonitorView', { treeDataProvider: nvidiaMonitorTreeDataProvider });
 
     const refreshCommand = vscode.commands.registerCommand('nvidia-monitor.refresh', () => {
@@ -148,11 +158,10 @@ function activate(context) {
         vscode.window.showInformationMessage('NVIDIA Monitor data refreshed!');
     });
 
-    // ตั้งเวลาอัปเดตข้อมูลทุก 5 วินาที
     const interval = setInterval(() => {
         nvidiaMonitorTreeDataProvider.refresh();
         console.log('NVIDIA Monitor data auto-refreshed');
-    }, 500);
+    }, 1000);
 
     context.subscriptions.push(refreshCommand);
     context.subscriptions.push({
@@ -160,7 +169,7 @@ function activate(context) {
     });
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
     activate,
